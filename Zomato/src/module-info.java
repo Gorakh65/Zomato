0db7952360426c0b174bb6f76ba1fff96f57bdef
gs empire <gsempire65@gmail.com>
2023-04-04 12:23:25 +0530

    Zomato loginPage Commit
    We work on Zomato LoginPage

---
+++ Zomato/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author MASS PROMOTIONS
+ *
+ */
+module Zomato {
+}
---
+++ Zomato/src/pageLayer/LoginPage.java
@@ -0,0 +1,17 @@
+package pageLayer;
+
+public class LoginPage {
+	public void ram() {
+		System.out.println("WelCome to GitHub");
+	}
+	public void sham() {
+		System.out.println("Eclipse");
+	}
+	public static void main(String[] args) {
+		
+		LoginPage a = new LoginPage();
+		a.ram();
+		a.sham();
+		
+	}
+}
