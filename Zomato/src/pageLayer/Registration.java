a76ac8a67075bd46edff01e5d4ed476c8066e7b4
gs empire <gsempire65@gmail.com>
2023-04-04 12:34:11 +0530

    Registration page commit
    We complete testing on Registration page

---
+++ Zomato/src/pageLayer/Registration.java
@@ -0,0 +1,23 @@
+package pageLayer;
+
+public class Registration {
+	
+	public void a () {
+		System.out.println("Red");
+	}
+   public void b() {
+	   System.out.println("Orange");
+	   
+   }
+   public void c() {
+	   System.out.println("Green");
+   }
+   public static void main(String[] args) {
+	
+	   Registration ss = new Registration();
+	   ss.a();
+	   ss.b();
+	   ss.c();
+	   
+}
+}
